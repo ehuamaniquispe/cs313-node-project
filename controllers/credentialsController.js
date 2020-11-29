ea689c62a05bb253ab2b468a11b54477407928d5
mhvsoftware <creaproyecto@hotmail.com>
2020-11-28 21:35:09 -0500

    2

--- controllers/credentialsController.js
+++ controllers/credentialsController.js
@@ -4,12 +4,11 @@ let checkCredentials = (req, res)=>{
     console.log("checking credentials ...");
     let userName = req.body.userName; // we use req.body because is a POST request
     let pass = req.body.pass;
-    // console.log(userName);
-    // console.log(pass);
+    
     let results = credentialsModel.checkCredentials(userName,pass,(error,result)=>{
         // res.json(result);
         if(result == 1){
-            res.sendFile('/public/expenses.html');
+            res.sendFile('./public/expenses.html');
         }
 
     });
