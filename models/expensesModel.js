54073674d2c7a470e6360af58cbe6d699500db59
mhvsoftware <creaproyecto@hotmail.com>
2020-12-09 20:08:27 -0500

    5

--- models/expensesModel.js
+++ models/expensesModel.js
@@ -3,7 +3,7 @@ const db_url = process.env.DATABASE_URL;
 const pool = new Pool({connectionString:db_url});
 
 let getAllExpenses = (callback)=>{
-   let sql = "SELECT * FROM expenses"; 
+   let sql = "SELECT * FROM expenses INNER JOIN familymember ON expenses.familymember_idfamilymember = familymember.idfamilymember"; 
    pool.query(sql,(err,db_result)=>{
        console.log(db_result.rows);
        callback(null,db_result.rows);
