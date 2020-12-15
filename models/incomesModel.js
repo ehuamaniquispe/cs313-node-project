39576cff75de110b44674b996dd46b1bdb8d569a
mhvsoftware <creaproyecto@hotmail.com>
2020-12-15 12:25:29 -0500

    6

--- models/incomesModel.js
+++ models/incomesModel.js
@@ -2,7 +2,7 @@ const{Pool} = require ("pg");
 const db_url = process.env.DATABASE_URL;
 
 //inserting incomes
-let insertIncome=(description,amount,date,userId)=>{
+let insertIncome=(description,amount,date,userId,callback)=>{
     let sql = "INSERT INTO incomes (incomes_description,incomes_amount, incomes_date,familymember_idfamilymember) VALUES($1,$2,$3,$4)";
     let values = [description,amount,date,userId]; 
     pool.query(sql,values,(err,db_result)=>{
