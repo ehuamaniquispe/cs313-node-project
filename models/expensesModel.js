aac1bcaf72b21223585f25468215bf6cbf2f1011
mhvsoftware <creaproyecto@hotmail.com>
2020-12-14 23:13:26 -0500

    6

--- models/expensesModel.js
+++ models/expensesModel.js
@@ -16,7 +16,7 @@ let insertNewExpense = (description,amount,userId, callback) =>{
     //create the new topic in the DB
     let date = new Date();
     let sql = "INSERT INTO expenses(expenses_description,expenses_amount,expenses_date,familymember_idfamilymember) VALUES ($1,$2,$3,$4)"; 
-    let values = [description,amount,date,userId ];
+    let values = [description,amount,date,userId];
     pool.query(sql,values,(err,db_result)=>{
         if(err){
             throw err
